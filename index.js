cd8489d30ed0353bc3055dcbadd7c30758e7a147
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-06 01:45:01 +0600

    data cullect from client side by using post method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,20 @@ async function run() {
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
+        });
+
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        });
+
+        // POST
+        app.post('/items', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemsCollection.insertOne(newItem);
+            res.send(result);
         })
     }
     finally{}
