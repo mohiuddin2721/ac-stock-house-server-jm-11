cf9b57e0be9712c607b073ceeac0eea76444f406
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-07 17:38:06 +0600

    delete api created for myItem delete

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
             res.send(myItems);
         });
 
+        // My Item DELETE
+        app.delete('/myItems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        });
+        
     }
     finally{}
 }
